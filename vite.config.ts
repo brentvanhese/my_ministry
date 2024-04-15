90bcc39f62fd5137fcdad7a4f6986b1f37a31ed4
Brent Van Hese <brent.vanhese@icapps.com>
2024-04-15 21:49:37 +0200

    added scripts for manual deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
-import path from 'path';
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "my_ministry",
   plugins: [
     react(),
     VitePWA({
